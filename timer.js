4ba46f2bec137116e0596886a4eee830ca347ce3
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-12-05 11:00:03 +0530

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,10 @@
+function showtime(){
+    const date = new Date();
+    return date.getHours()+ "Hrs "+date.getMinutes()+" mins:"+date.getSeconds+"secs";
+}
+function showSessionExpire(){
+    console.log("Activity-B: your session expired at "+showtime());
+}
+console.log("Activity-A: triggerring activity-B at "+showtime())
+setTimeout(showSessionExpire,5000)
+console.log("Activity-A triggerring activity-B at "+showtime()+"will execute after 5 second")
